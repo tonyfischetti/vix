ea5e343c773f4cde1b45fa73676a5103a87f1d02
Tony Fischetti <tony.fischetti@gmail.com>
2022-11-16 15:16:56 -0500

    added plug thing to install file

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
 reset
 cd plugged/coc.nvim
