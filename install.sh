e169448627327aece7899cc1b88cee375d2eaf06
Tony Fischetti <tony.fischetti@gmail.com>
2022-11-16 13:19:07 -0500

    changed install script

--- install.sh
+++ install.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-INITFILE="~/.config/nvim/init.vim"
+nvim +PlugInstall +qall
+cd plugged/coc.nvim
+yarn install
 
-REPOS=`cat ~/.config/nvim/init.vim | ack '^Plug' | perl -pe "s/Plug '(.+)'/\1/"`;
-
-echo $REPOS;
-
-IFS=$'\n';
-
-# mkdir -p ~/.vim/bundle;
-# cd bundle;
-#
-# for repo in $REPOS; do
-#     `git clone https://github.com/$repo`;
-# done
-#
 # echo "Make sure you run ':CocInstall coc-json coc-tsserver coc-docker coc-deno"
