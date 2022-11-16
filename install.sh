9688a2a506ecfc7a03c80024692a0669966e5b6e
Tony Fischetti <tony.fischetti@gmail.com>
2022-11-16 14:15:55 -0500

    added to install script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 nvim +PlugInstall +qall
+reset
 cd plugged/coc.nvim
 yarn install
+ln -s $HOME/.config/nvim ~/.vim
 
 # echo "Make sure you run ':CocInstall coc-json coc-tsserver coc-docker coc-deno"
