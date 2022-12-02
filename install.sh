762d860e4492ede34c93f33ca655610d6044a23e
Tony Fischetti <tony.fischetti@gmail.com>
2022-12-02 15:57:16 -0500

    experimenting with autoinstall script

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +PlugInstall +qall
-reset
-cd plugged/coc.nvim
-yarn install
+nvim --headless +PlugInstall +qall
 ln -s $HOME/.config/nvim ~/.vim
 
-# echo "Make sure you run ':CocInstall coc-json coc-tsserver coc-docker coc-deno"
