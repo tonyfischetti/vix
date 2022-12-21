be5816fffb4e7aa38adef53fdf9d5c210e2ac7da
Tony Fischetti <tony.fischetti@gmail.com>
2022-12-21 14:06:20 -0500

    safer install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim --headless +PlugInstall +qall
 ln -s $HOME/.config/nvim ~/.vim
