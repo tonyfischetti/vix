dfaace2a6a60a4167dc13a2559d367af4983c339
Tony Fischetti <tony.fischetti@gmail.com>
2024-03-26 16:57:17 -0400

    added (and fixed) devicons

--- lua/vix/plugins/devicons.lua
+++ lua/vix/plugins/devicons.lua
@@ -1,8 +1,7 @@
 require('nvim-web-devicons').setup {
-    -- your personal icons can go here (to override)
-    -- you can specify color or cterm_color instead of specifying both of them
-    -- DevIcon will be appended to `name`
     default = true,
+    color_icons = true,
+    -- strict = true,
     override = {
         lisp = {
             icon = "λ",
@@ -28,17 +27,22 @@ require('nvim-web-devicons').setup {
             name = "dockerfile"
         },
         text = {
-            icons = "",
+            icon = "",
             color = "#deaded",
             name = "text"
         },
+        ps1 = {
+            icon = "",
+            color = "#428850",
+            name = "ps1"
+        },
         tsv = {
-            icons = "",
+            icon = "",
             color = "#428850",
             name = "Csv"
         },
         default = {
-            icons = "",
+            icon = "",
             color = "#deaded",
             name = "text"
         }
@@ -58,7 +62,12 @@ require('nvim-web-devicons').setup {
             icon = "",
             color = "#FFAF87",
             name = "dockerfile"
-        }
+        },
+        ["NEWS"] = {
+            icon = "",
+            color = "#FFAF87",
+            name = "News"
+        },
     },
     override_by_extension = {
         ["txt"] = {
@@ -71,16 +80,109 @@ require('nvim-web-devicons').setup {
             color = "#f1502f",
             name = "text"
         },
+        ["m4"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "m4"
+        },
+        ["in"] = {
+            icon = "",
+            color = "#428850",
+            name = "autotools"
+        },
+        ["ac"] = {
+            icon = "",
+            color = "#428850",
+            name = "autotools"
+        },
+        ["asc"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "gpg"
+        },
+        ["gpg"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "gpg"
+        },
+        ["dat"] = {
+            icon = "",
+            color = "#428850",
+            name = "Data"
+        },
+        ["asd"]= {
+            icon = "λ",
+            color = "#428850",
+            cterm_color = "65",
+            name = "lisp"
+        },
         ["tsv"] = {
-            icons = "",
+            icon = "",
             color = "#428850",
             name = "tsv"
-        }
-    },
-    color_icons = true;
+        },
+        ["pdf"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "PDF"
+        },
+        ["xz"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "archive"
+        },
+        ["gz"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "archive"
+        },
+        ["zstd"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "archive"
+        },
+        ["bz2"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "archive"
+        },
+        ["zpaq"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "archive"
+        },
+        ["tar.xz"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "archive"
+        },
+        ["tar.gz"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "archive"
+        },
+        ["tar.bz2"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "archive"
+        },
+        ["deb"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "Debian"
+        },
+        ["patch"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "patch"
+        },
+        ["AppImage"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "AppImage"
+        },
+    }
 }
 
 require("nvim-web-devicons").set_default_icon('', '#6d8086', 65);
 
--- require("nvim-web-devicons").set_icon_by_filetype { tsv = "csv" }
-require("nvim-web-devicons").set_icon_by_filetype { csv = "tsv" };
