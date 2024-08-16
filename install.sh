2f248fcd6c32eed22a24e4fa8200d6e7265a5db5
Tony Fischetti <tony.fischetti@gmail.com>
2024-08-16 18:32:37 -0400

    checkpoint

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # set -euxo pipefail
 
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -Lo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim --create-dirs 
 nvim --headless +PlugInstall +qall
 ln -s $HOME/.config/nvim ~/.vim
 
