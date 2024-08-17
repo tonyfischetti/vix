5f1dcc03c3c0bc4f3ec9193b72349c943cbbb648
Tony Fischetti <tony.fischetti@gmail.com>
2024-08-17 13:35:26 -0400

    progress

--- install.js
+++ install.js
@@ -1,96 +1,12 @@
 #!/usr/bin/env node
 
 
+
 import * as os            from 'node:os';
 import * as fs            from 'node:fs/promises';
 import * as path          from 'node:path';
 import { Command }        from 'commander';
 import { createConsola }  from "consola";
-
-
-
-/*****************************************************************
- * Globals
- */
-const DESCRIPTION = "Install vix (a specific neo-vim environment)";
-const VERSION = "0.0.1";
-let   DEBUG = false;
-let   consola = null;
-const PLUG_URL = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
-const PROGRAM = new Command();
-
-
-/*****************************************************************
- * CLI argument parsing
- */
-PROGRAM.
-  name("install.js").
-  description(DESCRIPTION).
-  version(VERSION);
-
-PROGRAM.
-  option("-d, --debug", "turn on DEBUG");
-
-PROGRAM.parse(process.argv);
-
-const OPTS = PROGRAM.opts();
-if (OPTS.debug) { DEBUG = true; }
-
-consola = createConsola({ level: 3 + DEBUG });
-const { box } = consola;
-
-
-/*****************************************************************
- * Error functions
- */
-const printErrorAndBailOut = (exitCode, customMessage) => {
-  return (error) => {
-    customMessage ? console.error(customMessage) : console.error(error.message);
-    if (DEBUG) console.error(error);
-    process.exit(exitCode);
-  };
-};
-
-const fatalCantFindPrefix  = printErrorAndBailOut(1);
-
-
-/*****************************************************************
- * Info / logging functions
- */
-const tee = (arg) => { console.log(arg); return arg; }
-
-const info = (message, fn=consola.info, label) => {
-  return (arg) => { fn(message ?? `${label ?? "debug"}: ${arg}`); return arg; }
-};
-
-const debug = (message, fn=consola.debug) => {
-  return (arg) => { fn(`${message ?? "debug: "}${arg}`); return arg; }
-};
-
-
-/*****************************************************************
- * Main functions
- */
-const getNvimPrefix = () => {
-  const prefix = process.env["XDG_DATA_HOME"] ??
-                   `${os.homedir()}/.local/share/` ?? "";
-  if (prefix) return prefix;
-  throw Error("couldn't find nvim prefix");
-}
-
-const formDownloadOutputPath = (prefix) => {
-  const PLUG_SUBPATH = "/nvim/site/autoload/";
-  return path.join(prefix, PLUG_SUBPATH);
-};
-
-const formPlugPath = (parentDir) => `${parentDir}/plug.vim`;
-
-const mkdirDashP = (subdirectory) => {
-  return fs.mkdir(subdirectory, { recursive: true });
-};
-
-
-
 
 
 /*****************************************************************
@@ -122,7 +38,7 @@ const StateFlow = () => {
       console.log("peepee");
       return wrapPromise(promise.then(flow));
     };
-    return wrapPromise(promise);
+    return promise;
   };
 
   return wrapPromise(apromise);
@@ -132,19 +48,133 @@ const StateFlow = () => {
 
 
 
+
+/*****************************************************************
+ * Globals
+ */
+const DESCRIPTION = "Install vix (a specific neo-vim environment)";
+const VERSION = "0.0.1";
+let   DEBUG = false;
+let   consola = null;
+const PROGRAM = new Command();
+
+
+/*****************************************************************
+ * CLI argument parsing
+ */
+PROGRAM.
+  name("install.js").
+  description(DESCRIPTION).
+  version(VERSION);
+
+PROGRAM.
+  option("-d, --debug", "turn on DEBUG");
+
+PROGRAM.parse(process.argv);
+
+const OPTS = PROGRAM.opts();
+if (OPTS.debug) { DEBUG = true; }
+
+consola = createConsola({ level: 3 + DEBUG });
+const { box, done } = consola;
+
+
+/*****************************************************************
+ * Error functions
+ */
+const printErrorAndBailOut = (exitCode, customMessage) => {
+  return (error) => {
+    customMessage ? console.error(customMessage) : console.error(error.message);
+    if (DEBUG) console.error(error);
+    process.exit(exitCode);
+  };
+};
+
+const fatalCantFindPrefix     = printErrorAndBailOut(1);
+const fatalCantDownloadPlug   = printErrorAndBailOut(2, "Can't download Plug");
+const fatalCantWriteFile      = printErrorAndBailOut(2, "Can't write file");
+
+
+/*****************************************************************
+ * Info / logging functions
+ */
+const tee = (arg) => { console.log(arg); return arg; }
+
+const info = (message, fn=consola.info, label) => {
+  return (arg) => { fn(message ?? `${label ?? "debug"}: ${arg}`); return arg; }
+};
+
+const debug = (message, fn=consola.debug) => {
+  return (arg) => { fn(`${message ?? "debug: "}${arg}`); return arg; }
+};
+
+
+/*****************************************************************
+ * Helper functions
+ */
+
+
+/*****************************************************************
+ * Main functions
+ */
+const getNvimAutoloadPrefix = () => {
+  const prefix = process.env["XDG_DATA_HOME"] ?? 
+                   `${os.homedir()}/.local/share/` ?? "";
+  if (prefix) return prefix;
+  throw Error("couldn't find nvim prefix");
+}
+
+const formDownloadOutputPath = (prefix) => {
+  const PLUG_SUBPATH = "/nvim/site/autoload/";
+  return path.join(prefix, PLUG_SUBPATH);
+};
+
+const formPlugPath = (parentDir) => `${parentDir}/plug.vim`;
+
+const mkdirDashP = (subdirectory) => {
+  return fs.mkdir(subdirectory, { recursive: true }).
+    then(_ => subdirectory);
+};
+
+const downloadPlug = (downloadTo) => {
+  const plugURL = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
+  const extractText = (response) => response.text();
+  const returnBothPathAndText = (text) => { return {downloadTo, text} };
+  return fetch(plugURL).
+    then(extractText).catch(fatalCantDownloadPlug).
+    then(returnBothPathAndText);
+};
+
+const writePlugFile = ({downloadTo, text}) => fs.writeFile(`${downloadTo}/plug.vim`, text, 'utf-8');
+
+
+
+
+
+
+/*****************************************************************
+ * First-order chains
+ */
+const downloadPlugBootstrapper = () => {
+  return Promise.resolve().
+    then(getNvimAutoloadPrefix).catch(fatalCantFindPrefix).
+    then(debug("prefix: ")).
+    then(formDownloadOutputPath).
+    then(mkdirDashP).
+    then(debug("path: ")).
+    then(downloadPlug).
+    then(writePlugFile).catch(fatalCantWriteFile);
+};
+
+
+
 /*****************************************************************
  * Main
  */
-StateFlow().
-  metah(info("Installing vix", box)).
-  metah(getNvimPrefix).catch(fatalCantFindPrefix).
-  then(info("found prefix")).
-  then(debug("prefix: ")).
-  then(formDownloadOutputPath).
-  then(info("found prefix. making directory")).
-  then(mkdirDashP).
-  then(debug("path: ")).
-  then(_ => "peepee/poopoo");
+Promise.resolve().
+  then(info("Installing vix", box)).
+  then(downloadPlugBootstrapper).
+  then(info("done", done));
   
 
 
@@ -256,6 +286,8 @@ RangeError: Maximum call stack size exceeded
 
  ***************************************************************************
  ***************************************************************************/
+
+ composing promises
 
 
 `
