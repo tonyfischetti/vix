f73fccada6f7455c8cd5aa201b4f0fadadf639be
Tony Fischetti <tony.fischetti@gmail.com>
2025-07-10 14:08:01 -0400

    added data.table solution to a specific r problem

--- codex/r/specific/Mutate column conditionally.R
+++ codex/r/specific/Mutate column conditionally.R
@@ -1,5 +1,12 @@
 # r, mutate, mutate_if, mutate column conditionally
 
+# --- data.table ---
+char_cols <- names(dt)[sapply(dt, is.character)]
+dt[, (char_cols) := lapply(.SD, toupper), .SDcols=char_cols]
+
+
+# --- tidyverse ---
 DT %>%
   dplyr::mutate_if(is.character, toupper)
+
 
