f9adc17f438a709ec34f1458189d15dcf184297a
Tony Fischetti <tony.fischetti@gmail.com>
2025-11-21 11:22:09 -0500

    finally fixed niji

--- lua/vix/plugins/niji.lua
+++ lua/vix/plugins/niji.lua
@@ -1,13 +1,24 @@
 
 return {
 
-  --  TODO  doesn't start when it's support to
   {
     "losingkeys/vim-niji",
-    lazy = false,
+    lazy = true,
     init = function()
       vim.g.niji_matching_filetypes = {'lisp', 'scheme', 'clojure', 'racket'}
     end,
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {"lisp", "scheme", "racket", "clojure"},
+        callback = function()
+          -- Defer to ensure plugin is fully loaded
+          vim.schedule(function()
+            vim.cmd('call niji#highlight()')
+          end)
+        end,
+      })
+    end,
+    ft = { "lisp", "scheme", "racket", "clojure" },
     keys = {
       {
         "<Leader>P", function() vim.cmd(':call niji#highlight()') end
