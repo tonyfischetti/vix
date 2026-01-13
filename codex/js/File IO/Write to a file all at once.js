69020ecd0c757d1410b5118577597f309e1541d6
Tony Fischetti <tony.fischetti@gmail.com>
2026-01-13 08:38:14 -0500

    good changes on traveler

--- codex/js/File IO/Write to a file all at once.js
+++ codex/js/File IO/Write to a file all at once.js
@@ -2,13 +2,13 @@
 
 const fs = require("node:fs/promises");
 
-const inputFile = process.argv[2];
+const outputFile = process.argv[2];
 
 // will create a file if it doesn't exists
-fs.appendFile(inputFile, "  - Mad Hatter", 'utf-8').
+fs.appendFile(outputFile, "  - Mad Hatter", 'utf-8').
   then(() => { console.log("done!"); });
 
 // or
 
-fs.writeFile(inputFile, "  - Mad Hatter", 'utf-8').
+fs.writeFile(outputFile, "  - Mad Hatter", 'utf-8').
   then(() => { console.log("done!"); });
